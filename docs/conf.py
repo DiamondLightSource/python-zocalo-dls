1ac9bf82b1d24a7812828d4a916dad7d00d5f653
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-07-30 16:53:24 +0100

    Bump version: 0.1.0 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ author = u"Diamond Light Source"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.1.0"
+version = "0.2.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
