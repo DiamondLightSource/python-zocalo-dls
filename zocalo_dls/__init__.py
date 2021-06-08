0a20b8e5d4062cf4467299e32bf8eebb91751fa3
Jacob Filik <jacob.filik@diamond.ac.uk>
2021-06-08 09:29:23 +0100

    Bump version: 0.2.0 → 0.3.0

--- zocalo_dls/__init__.py
+++ zocalo_dls/__init__.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import, division, print_function
 
 import logging
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 logging.getLogger("zocalo_dls").addHandler(logging.NullHandler())
