75ba6390e21b7ff042b49e2486ead9fcaaa759a5
Jacob Filik <jacob.filik@diamond.ac.uk>
2021-06-08 09:54:55 +0100

    Bump version: 0.2.0 → 0.3.0 (#6)

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/DiamondLightSource/python-zocalo-dls",
-    version="0.2.0",
+    version="0.3.0",
     zip_safe=False,
 )
