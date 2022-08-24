670a28ab4a3ac773b1877514d73f1fb6d4c2dfbd
Jacob Filik <jacob.filik@diamond.ac.uk>
2022-08-24 08:53:13 +0100

    Bump version: 0.3.0 → 0.4.0 (#12)

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ author = u"Diamond Light Source"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.3.0"
+version = "0.4.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
